d12205ffa4f2b3613f0d6ab1da9c59f98d0fa7dd
Aochol <aocholayoul9@gmail.com>
2024-06-05 14:51:30 +0200

    working on footer and hearder

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
-
+import "./footer.scss";
+import {
+  FaFacebook,
+  FaInstagramSquare,
+  FaYoutube,
+  FaTwitter,
+  FaCcMastercard,
+  FaCcVisa,
+  FaCcPaypal,
+} from "react-icons/fa";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="left">
+        <h3>About us</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          repellat facere molestiae ab facilis sunt id. Non eligendi, fugit
+          harum sint, iure molestias, cumque laboriosam eius deleniti assumenda
+          odit perspiciatis.
+        </p>
+        <div className="social-icons">
+          <FaFacebook className="facebook" />
+          <FaInstagramSquare className="insta" />
+          <FaTwitter className="twitter" />
+          <FaYoutube className="youtube" />
+        </div>
+      </div>
+      <div className="middle">
+        <h3>News letter</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+          ullam perferendis fuga dolorem eveniet expedita voluptatibus qui
+        </p>
+        <div className="new-input">
+          <input
+            type="text"
+            placeholder="Enter your E-mail and get 10$ catch"
+          />
+          <button>Subscribes</button>
+        </div>
+        <div className="vias-icon">
+          <FaCcMastercard />
+          <FaCcVisa />
+          <FaCcPaypal />
+        </div>
+      </div>
+      <div className="right"></div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,20 +1,47 @@
-import './header.scss'
-
+import "./header.scss";
+import { Link } from "react-router-dom";
+import { FaUser,FaShoppingCart ,FaSearch} from "react-icons/fa";
 const Header = () => {
-  const menu = ["Home", "About", "Shop", "Blog", "Contact"];
+  const menu = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "About",
+      link: "/about",
+    },
+    {
+      name: "Shop",
+      link: "/shop",
+    },
+    {
+      name: "Blog",
+      link: "/blog",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+    },
+  ];
   return (
     <header>
       <nav>
         <div className="logo">
-          <h2> SleekStyle</h2>
+          <h2> <Link to="/" className="link">SleekStyle </Link> </h2>
         </div>
         <ul>
-          {menu.map((item) => (
+          {menu.map(({ name, link }) => (
             <li>
-              {item}
+              <Link to={link} className="link">{name}</Link>
             </li>
           ))}
         </ul>
+        <div className="right">
+          <div className="search"><FaSearch /></div>
+          <div className="user"><FaUser/></div>
+          <div className="cart"><FaShoppingCart /></div>
+        </div>
       </nav>
     </header>
   );
